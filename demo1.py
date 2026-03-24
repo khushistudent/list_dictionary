b6abd9d964f00fb7748f5055341bb570db9b104f
Khushi Dwivedi <khushidwivedi2240@gmail.com>
2026-03-24 14:45:33 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,12 @@
+# programed By : Khushi
+# Roll No. 36
+#List Assignment number : 1
+#Create a list of 5 friend names. Print the first and last name using positive and negative indexing
+name=["Purple","Orange","Papaya","Banana","Apple"]
+#use positive
+print("First name",name[0])
+print("last name",name[4])
+#use negative
+print("==========================================")
+print("First name",name[-1])
+print("last name",name[-5])
